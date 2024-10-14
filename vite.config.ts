2ba22905035a4ecf2918afd404762513ab25ade1
yumachin <curryuma925@gmail.com>
2024-10-14 09:20:56 +0900

    nineth commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Vite App',
+        short_name: 'ViteApp',
+        description: 'My awesome Vite app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/Jankara.jpg',
+            sizes: '400x400',
+            type: 'image/jpeg'
+          },
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff'
+      }
+    })
+  ],
 })
